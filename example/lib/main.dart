241c88cccc99dd56ebdea9640693fd4eef5b34a1
kev-techi <kevin@techi.be>
2025-09-22 17:27:34 +0200

    flywind example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'example.cupertino.dart';
+import 'example.cupertino.dart' as cupertino;
+import 'example.flywind.dart' as flywind;
 
 void main() {
-  runApp(const FlywindExample());
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _selectedExample = 'flywind';
+
+  @override
+  Widget build(BuildContext context) {
+    switch (_selectedExample) {
+      case 'flywind':
+        return const flywind.FlywindExample();
+      case 'cupertino':
+        return const cupertino.FlywindExample();
+      default:
+        return const flywind.FlywindExample();
+    }
+  }
 }
